d49fb7ef66b046ce7908b239b10a98bd2aed0825
Sean Scanlan <sean.scanlan@kettlenyc.com>
2016-02-03 17:10:18 -0500

    Cleaning up js a bunch. doens't actually work though, does it...

--- index.js
+++ index.js
@@ -1,51 +1,90 @@
 var express = require('express');
 var app = express();
-var slackToken = '';
-var todoList = [];
-var returnJson = {
-	'response_type': 'in_channel',
-	'text': 'Here are your Todos',
-	'attachments': [
-		{
-			'text': 'No todos!'
-		}
-	]
-};
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/'));
 
-app.get('/', function(req, res) {
-	if (req.query['token'] === slackToken) {
-		var request = req.query['text'];
-		if (request.indexOf('add') !== -1) {
-			// we add to the todolist
-			todoList.unshift(request.split('add ')[1]);
-		}
-		else if (request.indexOf('remove') !== -1) {
-			// we remove from the todolist
-			var indexInList = parseInt(request.split('remove ')[1], 10);
-			if (typeof(indexInList) == 'number') {
-				todoList.splice(indexInList - 1, 1);
+var SlackTodos = (function() {
+
+	var slackToken = ''
+	var todoList = [];
+	var channelIds = {};
+	var returnJson = {
+		'response_type': 'in_channel',
+		'text': 'Here are your Todos',
+		'attachments': [
+			{
+				'text': 'No todos!'
 			}
+		]
+	};
+
+	var listenToPort = function() {
+		app.listen(app.get('port'), function() {
+		  console.log("Node app is running at localhost:" + app.get('port'));
+		});
+	};
+
+	var setupListener = function() {
+		app.get('/', function(req, res) {
+			if (req.query['token'] === slackToken && false) {
+				var request = req.query['text'];
+				if (request.indexOf('add') !== -1) {
+					// we add to the todolist
+					addTask(req.query['channel_id'], request.split('add ')[1]);
+					res.send(channelIds.channelId);
+				}
+				else if (request.indexOf('remove') !== -1) {
+					// we remove from the todolist
+					var indexInList = parseInt(request.split('remove ')[1], 10);
+					if (typeof(indexInList) == 'number' && channelIds.channelId !== undefined) {
+						removeTask(channelId, indexInList);
+						res.send(channelIds.channelId);
+					}
+					res.send('Ruh Roh! Looks like you asked to remove something that does not exist!');
+				}
+			}
+			else {
+				res.send('Ruh Roh! Something went wrong!');
+			}
+		});
+	};
+
+	var addTask = function(channelId, todoText) {
+		if (channelIds.channelId == undefined) {
+			channelIds.channelId = {
+				'response_type': 'in_channel',
+				'text': 'Here are your Todos',
+				'attachments': [
+					{
+						'text': ''
+					}
+				],
+				'todo_list': []
+			};
 		}
-		parseText();
-		res.send(returnJson);
-	}
-	else {
-		res.send('temporarily broken! please try again later');
-	}
-});
+		channelIds.channelId['todo_list'].unshift(todoText);
+		channelIds.channelId['attachments'][0]['text'] = '';
+		for (var i = 0; i < channelIds.channelId['todo_list'].length; i++) {
+			channelIds.channelId['attachments'][0]['text'] += ((i + 1) + ') ' + channelIds.channelId['todo_list'][i] + '\n');
+		}
+	};
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
-});
+	var removeTask = function(channelId, indexInList) {
+		channelIds.channelId['todo_list'].splice(indexInList - 1, 1);
+		channelIds.channelId['attachments'][0]['text'] = '';
+		for (var i = 0; i < channelIds.channelId['todo_list'].length; i++) {
+			channelIds.channelId['attachments'][0]['text'] += ((i + 1) + ') ' + channelIds.channelId['todo_list'][i] + '\n');
+		}
+	};
 
-var parseText = function(channelId) {
-	var i;
-	var todoListLen = todoList.length;
-	returnJson[channelId]['attachments'][0]['text'] = todoList.length !== 0 ? '' : 'No todos!';
-	for (i = 0; i < todoList.length; i++) {
-		returnJson[channelId]['attachments'][0]['text'] += ((i + 1) + ') ' + todoList[i] + '\n');
+	return {
+		init: function() {
+			listenToPort();
+			setupListener();
+		}
 	}
-};
+
+}());
+
+SlackTodos.init();
