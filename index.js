9f5c107f50b8a9e6ea1c9e95d8510cc1ed634024
Sean Scanlan <sean.scanlan@kettlenyc.com>
2016-02-05 12:08:23 -0500

    Fixing issues

--- index.js
+++ index.js
@@ -23,10 +23,11 @@ var SlackTodos = (function() {
 				request = req.query['text'];
 				userId = req.query['user_id'];
 				// Create the user response if undefined
-				if (UserIds.userId == undefined) {
-					UserIds.userId = {
+				if (UserIds[userId] == undefined) {
+					UserIds[userId] = {
 						'response_type': 'in_channel',
 						'text': 'Here are your Todos',
+						'userId': userId,
 						'attachments': [
 							{
 								'text': ''
@@ -35,19 +36,25 @@ var SlackTodos = (function() {
 						'todo_list': []
 					};
 				}
+				// If the request is to add a todo
 				if (request.indexOf('add') !== -1) {
 					// we add to the todolist
-					addTask(req.query['user_id'], request.split('add ')[1]);
-					res.send(UserIds.userId);
+					addTask(request.split('add ')[1]);
+					res.send(UserIds[userId]);
 				}
+				// If the request is to remove a todo
 				else if (request.indexOf('remove') !== -1) {
 					// we remove from the todolist
 					var indexInList = parseInt(request.split('remove ')[1], 10);
 					if (typeof(indexInList) == 'number') {
-						// Need to figure out how to remove properly
-						res.send(UserIds.userId);
+						removeTask(indexInList);
+						res.send(UserIds[userId]);
 					}
 					res.send('Ruh Roh! Looks like you asked to remove something that does not exist!');
+				}
+				// If the request is to view the todos
+				else if (request.indexOf('view') !== -1) {
+					res.send(UserIds[userId]);
 				}
 				else {
 					res.send('Sorry, that is not a valid request.');
@@ -59,24 +66,20 @@ var SlackTodos = (function() {
 		});
 	};
 
-	var addTask = function(userId, todoText) {
-		UserIds.userId['todo_list'].unshift(todoText);
-		UserIds.userId['attachments'][0]['text'] = '';
-		for (var i = 0; i < UserIds.userId['todo_list'].length; i++) {
-			UserIds.userId['attachments'][0]['text'] += ((i + 1) + ') ' + UserIds.userId['todo_list'][i] + '\n');
-		}
+	var addTask = function(todoText) {
+		UserIds[userId]['todo_list'].unshift(todoText);
+		regenerateTaskText();
 	};
 
-	var removeTask = function(userId, indexInList) {
-		UserIds.userId['todo_list'].splice(indexInList - 1, 1);
-		return
-		regenerateTaskText(userId);
+	var removeTask = function(indexInList) {
+		UserIds[userId]['todo_list'].splice(indexInList - 1, 1);
+		regenerateTaskText();
 	};
 
-	var regenerateTaskText = function(userId) {
-		UserIds.userId['attachments'][0]['text'] = '';
-		for (var i = 0; i < UserIds.userId['todo_list'].length; i++) {
-			UserIds.userId['attachments'][0]['text'] += ((i + 1) + ') ' + UserIds.userId['todo_list'][i] + '\n');
+	var regenerateTaskText = function() {
+		UserIds[userId]['attachments'][0]['text'] = '';
+		for (var i = 0; i < UserIds[userId]['todo_list'].length; i++) {
+			UserIds[userId]['attachments'][0]['text'] += ((i + 1) + ') ' + UserIds[userId]['todo_list'][i] + '\n');
 		}
 	}
 
